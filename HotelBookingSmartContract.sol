8a4e27092f7762e462295438aeb59bbf89e8e273
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-08-19 21:07:16 -0700

    first commit, ^0.6.0 version

---
+++ HotelBookingSmartContract.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.6.0;
+contract HotelBooking{
+    enum Statuses {Vacant, Occupied}
+    Statuses currentStatus;
+
+    event Occupy(address _occupant, uint _value);
+
+    address payable public owner;
+
+    constructor() public {
+        owner = msg.sender;
+        currentStatus = Statuses.Vacant;
+    }
+    modifier onlyWhileVacant {
+        require(currentStatus == Statuses.Vacant, "Currently occupied");
+        _;
+    }
+
+    modifier costs(uint _amount) {
+        require(msg.value >= _amount, "not enough eth provided");
+        _;
+    }
+    receive()external payable onlyWhileVacant costs(2 ether) {
+        currentStatus = Statuses.Occupied;
+        owner.transfer(msg.value);
+        emit Occupy(msg.sender, msg.value);
+    }
+}
